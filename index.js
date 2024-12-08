8a53048e25e656cac5d81726ac6aa449f8b87410
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-08 09:46:18 +0600

    Comment Client.connect() and other for Deploy in Varcel.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const campDB = client.db("FoundFusions").collection('campaign');
         const donationDB = client.db("FoundFusions").collection('donated collection');
@@ -95,8 +95,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -106,7 +106,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!')
 })
 
 app.listen(port, () => {
